71e6b15a34cccb5fd6adff97fc5e7eb7db4e40a6
Atman-Shah <atman1339@gmail.com>
2023-08-29 20:44:00 +0530

    Minor corrections

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
-if (process.emit.v.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     // We are in production - return the prod set of keys
     module.exports = require("./prod");
 } else {
