16b3814fee9a5a1b34e8218c0e3a56ac182b0a1d
Atman-Shah <atman1339@gmail.com>
2023-09-01 17:06:56 +0530

    74th Video -  Refactoring with Async/Await

--- server/services/passport.js
+++ server/services/passport.js
@@ -8,7 +8,7 @@ const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   // user.id is the id in the mongo database, not the googleId.
-  done(null, user.id); 
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
@@ -26,18 +26,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true, // This is to fix the google redirect URI mismatch error.
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // We already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // We don't have a user record with this ID, make a new record
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));  
-        }  
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // We already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+      // We don't have a user record with this ID, make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
