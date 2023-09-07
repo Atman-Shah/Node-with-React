339eacd9fb80f8023c264d25c945a850db9f1280
Atman-Shah <atman1339@gmail.com>
2023-09-07 15:47:31 +0530

    94th Video -  Additional Proxy Rules

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -3,12 +3,14 @@ import Router from "../Router";
 import Header from "./Header";
 
 const App = () => {
-    return (
-        <div className="App">
-            <Header />
-            <Router />
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="App">
+        <Header />
+        <Router />
+      </div>
+    </div>
+  );
 };
 
 export default App;
