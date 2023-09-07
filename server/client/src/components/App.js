15d9acf63664841a7b11c9bcdb29b68f2c38ab9b
Atman-Shah <atman1339@gmail.com>
2023-09-07 16:48:12 +0530

    97th Video -  Testing FetchUser

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "../Router";
 import Header from "./Header";
+import { fetchUser } from "../helper";
+import { authAtom } from "../states";
+import { useAtom } from "jotai";
 
 const App = () => {
+    useEffect(() => {
+        (async () => {
+            const user = await fetchUser();
+            console.log("User: ", user);
+        })();
+    }, []);
+
   return (
     <div className="container">
       <div className="App">
